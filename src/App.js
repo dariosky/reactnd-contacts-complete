734f116a4faf9c36876b60cceefd36a87e27d8a1
Dario Varotto <dariosky@gmail.com>
2017-08-05 11:10:46 +0200

    Search and using getall API

--- src/App.js
+++ src/App.js
@@ -1,47 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ContactList from './ContactList';
 
 class App extends Component {
-  constructor(props){
-    super()
-    this.state = {contacts: [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
-  };
-}
 
-deleteContact = (contact) => {
-  console.log("Deleting", contact.id);
-  this.setState((state)=>({
-    contacts:state.contacts.filter((c)=>c.id!==contact.id)
-  }))
-}
+  deleteContact = (contact) => {
+    console.log("Deleting", contact.id);
+    this.setState((state) => ({
+      //contacts:state.contacts.filter((c)=>c.id!==contact.id)
+    }))
+  }
 
 
-render() {
-  return (
-    <ContactList 
-    onDeleteContact={this.deleteContact}
-    contacts={this.state.contacts} />
-  )
-}
+  render() {
+    return (
+      <ContactList
+        onDeleteContact={this.deleteContact}
+        contacts={[]}
+      />
+    )
+  }
 }
 
 export default App;
--- src/ContactList.js
+++ src/ContactList.js
@@ -1,39 +1,104 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types'
+import escapeStringRegexp from 'escape-string-regexp'
+import sortBy from 'sort-by'
+import {getAll} from './utils/ContactsAPI';
 
 class ContactList extends Component {
-  render() {
-    return (<ol className="list-contacts">
-    {this.props.contacts.map((contact, index) => (
-      <Contact key={contact.id} contact={contact}
-      onDeleteContact={this.props.onDeleteContact}/>
-    ))
-  }
-  </ol>)
-}
-}
 
-ContactList.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired,
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      query: '',
+      contacts: null
+    };
+
+  }
+
+  updateQuery = (query) => {
+    this.setState({
+      query: query.trim()
+    })
+  };
+
+  static propTypes = {
+    onDeleteContact: PropTypes.func.isRequired,
+  };
+
+  componentDidMount() {
+    getAll().then((contacts) => {
+        this.setState({
+            contacts: contacts
+          }
+        )
+      }
+    )
+  }
+
+
+  render() {
+    console.log(this.state);
+    const {contacts, query} = this.state;
+    const {onDeleteContact} = this.props,
+      match = new RegExp(escapeStringRegexp(query), 'i');
+
+    if (contacts === null) {
+      return <div>Loading contacts...</div>
+    }
+
+    const filteredContact = query ? contacts.filter(
+      (e) => match.test(e.name)
+    ) : contacts;
+
+    return (
+      <div className="list-contacts">
+        <div className="list-contacts-top">
+          <input className="search-contacts"
+                 type="text"
+                 placeholder="Search contacts"
+                 value={this.state.query}
+                 onChange={(e) => {
+                   this.updateQuery(e.target.value)
+                 }}
+          />
+        </div>
+
+        {filteredContact.length !== contacts.length && (
+          <div className="showing-contacts">
+            Showing {filteredContact.length} out of {contacts.length}
+          </div>
+        )}
+
+        <ol className="list-contacts">
+          {filteredContact.sort(sortBy('name', 'email')).map((contact, index) => (
+            <Contact key={contact.id} contact={contact}
+                     onDeleteContact={onDeleteContact}/>
+          ))
+          }
+        </ol>
+      </div>
+    )
+  }
 }
 
 class Contact extends Component {
   render() {
-    const contact = this.props.contact, props=this.props;
-    return (
-      <li className="contact-list-item">
+    const {contact} = this.props;
+    const {onDeleteContact} = this.props;
+
+    return <li className="contact-list-item">
       <img className="contact-avatar" src={contact.avatarURL} alt=""/>
       <div className="contact-details">
-      <p style={{fontWeight: 'bold'}}>{contact.name}</p>
-      <p>{contact.email}</p>
+        <p style={{fontWeight: 'bold'}}>{contact.name}</p>
+        <p>{contact.email}</p>
       </div>
       <span className="contact-remove"
-      onClick={()=>{console.log(props);props.onDeleteContact(contact)}}
-      ></span>
-      </li>)
-    }
+            onClick={() => {
+              onDeleteContact(contact)
+            }}
+      />
+    </li>
   }
-  
-  export default ContactList
-  
+}
+
+export default ContactList
